7dbab883a8eeb17bd0ac83442f516ff76abd1bd8
Joshua Reyes <reyes.j15@gmail.com>
2022-03-31 19:43:38 -0400

    set up get route

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const path =require('path');
 const app = express();
+
+app.get('/', (req,res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
 const port = process.env.PORT || 3000;
 app.listen, (port, ()=> console.log(`listening on port ${port}`));
