6464b64933e06bdb4a653eb6e966be0e134a34fc
Joshua Reyes <reyes.j15@gmail.com>
2022-04-02 10:25:22 -0400

    react setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('hello we used webpack!!!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.querySelector('#root');
+ReactDOM.render(<hr />, root);
